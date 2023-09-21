16f4116dd6cc278c45ca4611ba18c567f7e0dbc7
Alex Teixeira da Fonseca <alexatf2014@gmail.com>
2023-09-21 20:04:14 -0300

    feat: second module

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,25 @@
 import fastify from 'fastify'
 import { knex } from './database'
 
+import crypto from 'node:crypto'
+import { env } from './env'
+
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Hello World',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transaction
 })
-
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333 🚀')
